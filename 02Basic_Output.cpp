c56a5ab94ebc6e325758160bac945dedf30163f0
JEZA <jezamariebetchayda11@gmail.com>
2024-10-04 12:29:24 +0800

    Add files via upload

---
+++ 02Basic_Output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << "OUR LADY OF LOURDES COLLEGE FOUMDATION" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT-1A" << endl;
+	cout << "Jeza Marie S. Betchayda" << endl;
+	cout << "P-1 Mabini, Capalonga" << endl;
+	return 0;
+}
