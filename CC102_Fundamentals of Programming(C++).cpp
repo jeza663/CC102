056e4c88b0c732fd484bca21a7f32d892f290645
JEZA <jezamariebetchayda11@gmail.com>
2024-10-15 10:38:58 +0800

    Add files via upload

---
+++ CC102_Fundamentals of Programming(C++).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;
+	double sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	
+	cout << "Enter quantity of the sugar";
+	cin >> sugarQty;
+	cout << "Enter price of the sugar";
+	cin >> sugarPriceUSD;
+	
+	cout << "Enter quantity of the rice";
+	cin >> riceQty;
+	cout << "Enter price of the rice";
+	cin >> ricePriceUSD;
+
+	cout << "Enter quantity of sardines";
+	cin >> sardinesQty;
+	cout << "Enter price of the sardines";
+	cin >> sugarPriceUSD;
+	
+	cout << "Enter quantity of the coffee";
+	cin >> coffeeQty;
+	cout << "Enter price of the coffee";
+	cin >> coffeePriceUSD;
+	
+	cout << "Enter quantity of the milk";
+	cin >> milkQty;
+	cout << "Enter price of the milk";
+	cin >> milkPriceUSD;
+	return 0;
+}
+	
+	
