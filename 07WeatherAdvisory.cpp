d1900cbeb76c1f742839fb7ce9564b213fa462c1
JEZA <jezamariebetchayda11@gmail.com>
2024-11-13 12:17:11 +0800

    Add files via upload

---
+++ 07WeatherAdvisory.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int temperature;
+	
+	// Promt user for the temperature
+	cout << "Enter the temperature: ";
+	cin >> temperature;
+	
+	// Conditions based on the temperature
+	if (temperature < 32) {
+		cout << "It's recommended to bring a heavy jacket." << endl;
+		} else if (temperature >= 32 && temperature <= 50) {
+		cout << "It's recommended to bring light jacket." << endl;
+		} else  {
+		cout << "No jacket needed." << endl;
+		}
+		return 0;
+	}
