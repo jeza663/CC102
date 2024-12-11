4d40db462c0a5297629f7db8ec84748eec8e1973
JEZA <jezamariebetchayda11@gmail.com>
2024-12-11 03:19:47 -0800

    Update 19Act2_Looping.cpp

--- 19Act_2Looping.cpp
+++ 19Act_2Looping.cpp
@@ -1,73 +1,95 @@
 #include <iostream>
-
 using namespace std;
 
-void pattern_a(int n){
-	for (int i = 0;i <n; ++i){
-		for (int j = 0;j <i; ++j){
-			cout << "#";
-		}
-		cout << endl;
-	}
-} 
-void pattern_b(int n){
-	for (int i = 0;i <n; ++i){
-		for (int j = 0;j <n; ++j){
-			if (j < n - i-1){
+int main() {
+	char symbol1, symbol2, symbol3;
+	// Pick symbols
+	cout << "Enter 1st symbol to use (*, ^, #, $, @): ";
+	cin >> symbol1;
+	cout << "Enter 2nd symbol to use (*, ^, $, @): ";
+	cin >> symbol2;
+	cout << "Enter 3rd symbol to use (*, $, @): ";
+	cin >> symbol3;
+	cout << "You picked: " << symbol1 << " " << symbol2 << " " << symbol3 << endl;
+
+	// f:
+	cout << "f:" << endl;
+	for (int i = 0; i < 10; 1++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == i) {
 				cout << " ";
 			} else {
-				cout << "#";
+				cout << symbol1;
 			}
 		}
 		cout << endl;
 	}
-}
-void pattern_c(int n){
-	for (int i = 0;i <n; ++i){
-		for (int j = 0;j <n-i; ++j){
+
+	// :g
+	cout << "g:" << endl;
+	for (int i = 0; i < 5; i++) {
+		if (i % 2 ! = 0) {
+			cout << " ";
+		}
+		for (int j = 0; j < 10; j++) {
+			cout << symbol2 << " ";
+		}
+		cout << endl;
+	}
+
+	// h: (combined symbol1 and symbol2)
+	cout << "h:" << endl;
+	for (int i = 1; i <=6; i++) {
+		for (int j = 1; j < i; j++) {
+			cout << symbol1;
+		}
+		cout << symbol2 << endl;
+	}
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 6; j++) {
+			cout << symbol1;
+		}
+		cout << symbol2 << endl;
+	}
+
+	for (int i = 6; i >= 1; i--) { 
+		for (int j = 1; j < i; j++) {
+			cout << symbol1;
+		}
+		cout << symbol2 << endl;
+	}
+
+	// i: (symbol1, symbol2, symbol3 combined)
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 1; j++) {
+			cout << "          ";
+			for (int k = 0; k < i; k++) {
+				cout << "^";
+			}
+		}
+		cout << "*" << endl;
+	}
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 10; j++) {
 			cout << "#";
 		}
-		cout << endl;
-	}
-}
-void pattern_d(int n){
-	for (int i = 1;i <=i; ++i){
-		for (int j = 1;j <=i; ++j){
-			cout <<j<<" ";
+		for (int j = 0; j < 5; j++) {
+			cout << "^";
 		}
-		cout << endl;
+		cout << "*" << endl;
 	}
-}
-void pattern_e(int n){
-	for (int i = 1;i <=n; ++i){
-		for (int j = 1;j <=i; ++j){
-			cout <<j<<" ";
+
+	for (int i = 4; i >= 0; i--) {
+		for (int j = 0; j < 1: j++) {
+			cout << "        ";
+			for (int k = 0; k < i; k++) {
+				cout << "^";
+			}
+
 		}
-		cout << endl;
+		cout << "*" << endl;
 	}
-	for (int i=j; i>=n; ++j){
-		for (int j=1; j<=i; ++j){
-			cout <<j<<" ";
-		}
-		cout << endl;
-	}
-}
-int main(){
-	int n = 8; // You can change this value to generate different sizes
-	cout << "Pattern (a):" << endl;
-	pattern_a(n);
-	cout << endl;
-	cout << "Pattern (b):" << endl;
-	pattern_b(n);
-	cout << endl;
-	cout << "Pattern (c):" << endl;
-	pattern_c(n);
-	cout << endl;
-	cout << "Pattern (d):" << endl;
-	pattern_d(n);
-	cout << endl;
-	cout << "Pattern (e):" << endl;
-	pattern_e(n);
-	cout << endl;
 	return 0;
 }
